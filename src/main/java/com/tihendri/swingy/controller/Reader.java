9899caf5bdb4acde4a3ddeafc11ee01e22c16640
tihendri <tihendri@student.wethinkcode.co.za>
2020-10-03 15:10:37 +0200

    A little touching up

--- src/main/java/com/tihendri/swingy/controller/Reader.java
+++ src/main/java/com/tihendri/swingy/controller/Reader.java
@@ -4,46 +4,6 @@ import java.io.*;
 
 public class Reader {
 
-//    public static String updatedStats;
-//
-//    public static void updatePlayersList(Character character) {
-//        try {
-//            File file = new File("Characters.txt");
-//            FileWriter fileWriter = new FileWriter(file);
-//            Scanner scanner = new Scanner(file);
-////            String[] elements = readLines();
-//            String delLine = null;
-//            String newLine;
-//            WriteToFile.removeLine(character);
-////            assert elements != null;
-////            for (String str : elements) {
-////                if (str.contains(character.getCharacter())) {
-////                    delLine = str;
-////                }
-////            }
-//            updatedStats = character.getStats().getType() + " " + character.getCharacterName() + " " + character.getStats().getLevel() + " " +
-//                    character.getStats().getAttack() + " " + character.getStats().getDefence() + " " +
-//                    character.getStats().getHitPoints() + " " + character.getStats().getXp() + " " +
-//                    character.getArtifact().getType().toUpperCase();
-////            while (scanner.hasNextLine()) {
-////            for (String str : elements) {
-////                String str = scanner.nextLine();
-////                if (str.contains(character.getCharacter())) {
-////                    WriteToFile.writeCharacters(updatedStats);
-////                    fileWriter.write(newLine + "\n");
-////                    break;
-////                }
-////                else {
-////                    fileWriter.write(str + "\n");
-////                }
-////            }
-////            }
-////            fileWriter.close();
-//        } catch (IOException e) {
-//            System.out.println("Faulty Characters file");
-//        }
-//    }
-
     public static String[] readLines() {
         try {
             File file = new File("Characters.txt");
@@ -69,7 +29,8 @@ public class Reader {
         int id = 0;
         int count = 1;
         elements = readLines();
-        System.out.println("Choose from listed players");
+        System.out.println("Choose your previously created character:");
+        System.out.println("_________________________________________\n");
         while (id < getLinesNumber()) {
             assert elements != null;
             System.out.println(count++ + ". " + elements[id++]);
